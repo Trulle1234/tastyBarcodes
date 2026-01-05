28e8d039bbc91707f2b6af677adfa758cb435f10
Trulle <leonard.trulsson@gmail.com>
2026-01-06 00:50:29 +0100

    its better but still broken

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from prompt_toolkit import prompt
 from openfoodfacts import API, APIVersion, Country, Environment, Flavor
 import menu
 import ingredients_list
+import threading
 
 api = API(
     user_agent="tastyBarcodes/v0.1 (boom@thetwoboom.xyz)",
@@ -56,23 +57,38 @@ elif choice == "Exit":
 
 clock = int(prompt("Set how many seconds the timer should have: " ).strip())
 
-input("Press Enter to continue...")
-while clock > 0:
+ingredient_en = random.choice(list(ingredients_list.ingredients_translated.keys()))
+ingredient_other = ingredients_list.ingredients_translated[ingredient_en]
+
+time_start = time.time()
+while time.time() - time_start < clock:
+    remaining_time = clock - int(time.time() - time_start)
     print("\n"*100)
-    print(f"Timer: {Fore.RED}{str(clock)}{Style.RESET_ALL}s left")
+    print(f"Timer: {Fore.RED}{remaining_time}{Style.RESET_ALL}s left")
 
-    ingredient_en = random.choice(list(ingredients_list.ingredients_translated.keys()))
-    ingredient_other = ingredients_list.ingredients_translated[ingredient_en]
+    print("Please find something containing: " + ingredient_en)
+    
+    code = [None]
+    def get_input():
+        code[0] = prompt("Please scan your item: ", default="").strip()
+    
+    thread = threading.Thread(target=get_input, daemon=True)
+    thread.start()
+    thread.join(timeout=5.0)
+    
+    if not code[0]:
+        continue
 
-    print("Plese find something contaning: " + ingredient_en)
-    code = input("Plese scan your item: ") # this is wrong but its just teporary
-
-    ingredients = api.product.get(code).get("ingredients")
-    if ingredient_en in api.product.get(code).get("ingredients"):
+    ingredients = api.product.get(code[0]).get("ingredients")
+    if ingredient_other in str(ingredients):
         print("You got it!")
+        points += 1
         
-    time.sleep(5)
-    clock -= 5
+        ingredient_en = random.choice(list(ingredients_list.ingredients_translated.keys()))
+        ingredient_other = ingredients_list.ingredients_translated[ingredient_en]
+    else:
+        print("You didn't get it")
+
 
 print(f"Timer: 0s left")
 print(f"You got {points} in total")
